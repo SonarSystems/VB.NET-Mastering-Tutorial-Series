af140d9984d8cdb4e151ee5ea035c74d1a3052f2
Sonar Systems <SonarSystems@users.noreply.github.com>
2018-09-24 14:07:58 +0100

    Code

---
+++ [2] Basics/Console Output/AwesomeProject/AwesomeProject/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.Write("Awesome" & " you are amazing ")
+        Console.WriteLine("Hello World")
+        Console.Write("Project")
+    End Sub
+
+End Module
