525b0d9306f75991724a8432268e58f8845e2be6
Sonar Systems <SonarSystems@users.noreply.github.com>
2018-10-05 15:38:25 +0100

    Code

---
+++ [4] Fiile Handling/File Handling/AwesomeProject/Module1.vb
@@ -0,0 +1,22 @@
+﻿Imports System.IO
+
+Module Module1
+
+    Sub Main()
+        Dim file As FileStream = New FileStream("File.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite)
+        Dim itr As Integer
+
+        ' Reading Code
+        For itr = 0 To file.Length
+            Console.WriteLine(file.ReadByte)
+        Next
+
+        ' Writing Code
+        'For itr = 0 To 100
+        '    file.WriteByte(CByte(itr))
+        'Next
+
+        file.Close()
+    End Sub
+
+End Module
