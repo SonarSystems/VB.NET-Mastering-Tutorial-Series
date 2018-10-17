fb1834dfa1313030abf047ab8f3117993ecc4da7
Sonar Systems <SonarSystems@users.noreply.github.com>
2018-10-17 14:53:12 +0100

    Code

---
+++ [7] Advanced/Modal Forms/FormApp/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim form As Form2 = New Form2()
+
+        form.ShowDialog()
+    End Sub
+End Class
