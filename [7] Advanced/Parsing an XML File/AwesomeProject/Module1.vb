12002713b2810bf19bf7152b6ecb9db8c5521a13
Sonar Systems <SonarSystems@users.noreply.github.com>
2018-10-18 15:38:44 +0100

    Code

---
+++ [7] Advanced/Parsing an XML File/AwesomeProject/Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Xml
+
+Module Module1
+    Sub Main()
+        Dim xr As XmlReader = XmlReader.Create("File.xml")
+        Do While xr.Read()
+            If xr.NodeType = XmlNodeType.Element AndAlso xr.Name = "owner" Then
+                Console.WriteLine(xr.ReadElementString)
+            ElseIf xr.NodeType = XmlNodeType.Element AndAlso xr.Name = "company" Then
+                Console.WriteLine(xr.ReadElementString)
+            Else
+                xr.Read()
+            End If
+        Loop
+    End Sub
+End Module
---
+++ [7] Advanced/Regular Expressions/AwesomeProject/Module1.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Text.RegularExpressions
+
+Module Module1
+
+    Sub Main()
+        Dim str As String = "Hello this is Frahaan"
+
+        Dim expression As String = "\bH\S*"
+
+        Dim matchC As MatchCollection = Regex.Matches(str, expression)
+        Dim loopy As Match
+
+        Dim counter As Integer = 0
+
+        For Each loopy In matchC
+            Console.WriteLine(loopy)
+            counter = counter + 1
+        Next
+
+        Console.WriteLine(counter)
+
+    End Sub
+
+End Module
